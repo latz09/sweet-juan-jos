3a80523d5fe0acb0f9483f9e57017c34c9d5f092
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-16 14:24:53 -0500

    Base layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+
+const Layout = ({children}) => {
+    return ( <div>
+        <Navigation />
+        {children}
+        <Footer />
+    </div> );
+}
+ 
+export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    
+    fontFamily: {
+			oswald: ['Oswald', 'sans-serif'],
+			// amatic: ['Amatic SC', 'sans-serif'],
+			amatic: ['Dancing Script', 'cursive'],
+		},
   },
   plugins: [],
 };
