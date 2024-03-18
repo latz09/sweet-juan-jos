6e428115016c9e70a565140cf8513939da271ccf
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-17 20:28:07 -0500

    hero layout darker layover

--- components/heros/HeroLayout.js
+++ components/heros/HeroLayout.js
@@ -12,7 +12,7 @@ const HeroLayout = ({ heading, subheading, img, CTA, CTAText }) => {
 				objectFit='cover'
 				className='z-0'
 			/>
-			<div className='absolute inset-0 bg-gradient-to-b from-dark/20 via-dark/70 to-dark/20  z-10'>
+			<div className='absolute inset-0 bg-gradient-to-b from-dark/30 via-dark/90 to-dark/30  z-10'>
 				<motion.div
 					className='flex flex-col justify-center items-center h-full'
 					initial={{ opacity: 0, scale: 0 }}
