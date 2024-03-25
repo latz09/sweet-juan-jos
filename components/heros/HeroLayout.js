6c86a570ac599fb88645e9b9950cb7b88745d3d4
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-25 18:59:07 -0500

    smaller hero layout

--- components/heros/HeroLayout.js
+++ components/heros/HeroLayout.js
@@ -5,7 +5,7 @@ import ButtonLink from '../utils/ButtonLink';
 
 const HeroLayout = ({ heading, subheading, img, CTA, CTAText }) => {
 	return (
-		<div className='relative h-[80vh] w-full text-light shadow-lg shadow-primary/40'>
+		<div className='relative h-[65vh] w-full text-light shadow-lg shadow-primary/40'>
 			<Image
 				src={img}
 				alt={heading}
@@ -15,15 +15,14 @@ const HeroLayout = ({ heading, subheading, img, CTA, CTAText }) => {
 			/>
 			<div className='absolute inset-0 bg-gradient-to-b from-dark/20 via-dark/70 to-dark/20  z-10 text-center'>
 				<motion.div
-					className='flex flex-col justify-center items-center h-full gap-2'
-					initial={{ opacity: 0, scale: .9 }}
+					className='flex flex-col justify-center items-center h-full gap-2 '
+					initial={{ opacity: 0, scale: 0.9 }}
 					whileInView={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 1.1, delay: 0.2 }}
 				>
-					<MainHeading title={heading} type='light'/>
+					<MainHeading title={heading} type='light' />
 					<SubHeading type='light' title={subheading} />
 					<div className='mt-10 '>
-				
 						<ButtonLink title={CTAText} type='secondary' href={CTA} />
 					</div>
 				</motion.div>
