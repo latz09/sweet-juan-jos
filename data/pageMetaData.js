9240fa411c683f0adf7ec2676697f048b9d65081
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-25 16:24:31 -0500

    basedescription for meta

--- data/pageMetaData.js
+++ data/pageMetaData.js
@@ -1,13 +1,14 @@
 const baseUrl = 'https://sweet-juan-jos.vercel.app';
 const ogImage = `${baseUrl}/images/stock/dessert-table.webp`;
-const baseKeywords = '';
+const baseKeywords = 'd';
+const baseDescription = 'sweet Juanjo\'s is where Stevens Point\'s celebrations get a sprinkle of fun! From weddings to birthdays, our cupcakes, cakes, and cookies are all about making your big moments a tad sweeter and a lot more memorable. Let\'s party, Wisconsin style!';
 
 
 export const pageMetadata = {
 	// HOME PAGE
 	'/': {
 		title: 'Sweet Jaunjos',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
@@ -15,42 +16,42 @@ export const pageMetadata = {
 	// Weddings and Special Events
 	'/services/weddings-and-special-events': {
 		title: 'Weddings & Special Events',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
 	// Cupcakes and Specialty Cakes
 	'/services/cupcakes-and-specialty-cakes': {
 		title: 'Cupcakes & Specialty Cakes',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
 	// Hand Crafted Cookies
 	'/services/hand-crafted-cookies': {
 		title: 'Hand Crafted Cookies',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
 	// Candy and Treats
 	'/services/candy-and-treats': {
 		title: 'Candy and Treats',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
 	// Contact Us
 	'/sweet-juanjos/contact-us': {
 		title: 'Contact Us - Sweet Juanjos',
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
 	// Documents
 	'/documents': {
 		title: "Documents - Sweet Juanjos",
-		description: '',
+		description: baseDescription,
 		ogImage: ogImage,
 		keywords: baseKeywords,
 	},
