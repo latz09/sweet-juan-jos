9bcbfb9b47bc0e24363079faae17d12ee57d3611
latz09 <85132328+latz09@users.noreply.github.com>
2024-05-02 16:52:35 -0500

    d

--- components/services/ServiceCards.js
+++ components/services/ServiceCards.js
@@ -6,11 +6,11 @@ import Link from 'next/link';
 
 const ServiceCards = () => {
 	return (
-		<div className=''>
-			<div className='grid place-items-center my-16'>
+		<div className='max-w-7xl mx-auto '>
+			<div className='grid place-items-center my-16 '>
 				<MainHeading title='Our Delicious Offerings' type='dark' />
 			</div>
-			<div className='grid lg:grid-cols-2 gap-24 max-w-7xl mx-auto place-items-center'>
+			<div className='grid lg:grid-cols-2 gap-24 place-items-center'>
 				{serviceCardsData.map((service) => (
 					<motion.div
 						key={service.id}
@@ -29,8 +29,8 @@ const ServiceCards = () => {
 							</motion.div>
 
 							{/* Overlay Container with Primary Background centered */}
-							<div className='absolute inset-0 flex items-end  justify-center  '>
-								<div className='bg-gradient-to-b w-full from-primary/70  to-primary/70  via-primary px-4 lg:px-16 py-6 shadow-lg shadow-primary/30 text-light text-center '>
+							<div className='absolute inset-0 flex items-center  justify-center  '>
+								<div className='bg-gradient-to-b  from-primary/70  to-primary/70  via-primary px-4 lg:px-16 py-4 shadow-lg shadow-primary/30 text-light text-center '>
 									
 									<SubHeading title={service.title} type='light' />
 								</div>
