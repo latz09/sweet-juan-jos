d75388573b9d9b725a9635f28091ec37efc08a3e
latz09 <85132328+latz09@users.noreply.github.com>
2024-05-24 16:37:19 -0500

    base metadata

--- components/utils/animations/PageEntry.js
+++ components/utils/animations/PageEntry.js
@@ -7,7 +7,7 @@ const PageEntry = ({ children, className }) => {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0 }}
-			transition={{ duration: 0.7 }}
+			transition={{ duration: 1.1 }}
             className={className}
 		>
 			{children}
