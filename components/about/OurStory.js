ed86e1563678f1c353879a88797f8f2bf445da7e
latz09 <85132328+latz09@users.noreply.github.com>
2024-05-31 13:35:05 -0500

    modal edit

--- components/about/OurStory.js
+++ components/about/OurStory.js
@@ -8,7 +8,7 @@ import ButtonLink from '../utils/ButtonLink';
 import { FaArrowLeft } from 'react-icons/fa6';
 
 const OurStory = ({ data }) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(true);
 
 	// Disable scrolling on the body when the modal is open
 	useEffect(() => {
@@ -41,19 +41,19 @@ const OurStory = ({ data }) => {
 						animate={{ y: 0 }}
 						exit={{ y: 2000 }}
 						transition={{ duration: 1.1 }}
-						className='fixed inset-0 bg-light/80 backdrop-blur z-50 flex justify-center items-center px-4 border-t-4 border-primary'
+						className='fixed inset-0 bg-light/80 backdrop-blur z-50 flex justify-center items-center  border-t-4 border-primary'
 						onClick={() => setIsOpen(false)}
 					>
 						<motion.div
 							// initial={{ opacity: 0 }}
 							// animate={{ opacity: 1 }}
 							// transition={{ duration: 1.1 }}
-							className='relative w-full h-full flex flex-col items-center justify-center p-2 lg:p-4 max-w-5xl '
+							className='relative w-full h-full flex flex-col items-center justify-center p-2 lg:p-4   '
 							onClick={(e) => e.stopPropagation()} // Prevent modal from closing when clicking inside
 						>
-							<div className='w-full h-full relative max-w-4xl mx-auto overflow-hidden '>
-								<div className='h-full flex flex-col lg:p-4 text-dark gap-8 overflow-y-auto max-h-[100vh] pb-16 scrollbar-hide'>
-									<div className='grid gap-7 lg:gap-10 place-items-center'>
+							<div className='w-full h-full relative  overflow-hidden  '>
+								<div className='h-full flex flex-col  text-dark gap-8 overflow-y-auto max-h-[100vh] pb-16 scrollbar-hide'>
+									<div className='grid gap-7 lg:gap-10 place-items-center '>
 										<button
 											onClick={() => setIsOpen(false)}
 											className='text-2xl my-1 lg:my-4 sticky top-0 text-primary bg-light/90 border border-primary/30 p-3 rounded-full shadow-lg shadow-primary/30'
@@ -71,17 +71,25 @@ const OurStory = ({ data }) => {
 											priority={true}
 											className='mx-auto shadow-lg shadow-primary/40 rounded-lg mb-4'
 										/>
-										{data.paragraphs.map((item, index) => (
-											<div
-												key={index}
-												className='mb-4 text-center w-full lg:w-5/6 grid gap-2'
-											>
-												<h2 className='text-2xl font-bold'>{item.heading}</h2>
-												<p className='text-xl lg:text-2xl font-semibold leading-8 lg:leading-10'>
-													{item.paragraph}
-												</p>
-											</div>
-										))}
+										<div className='grid place-items-center w-full '>
+											{data.paragraphs.map((item, index) => (
+												<div
+													key={index}
+													className={`${index % 2 === 0 ? 'bg-light/50' : 'bg-primary/10'} grid place-items-center py-12 w-full`}
+												>
+													<div
+														className={`text-center w-full lg:w-1/2 grid gap-2 ${index % 2 === 0 ? '' : ''}`}
+													>
+														<h2 className='text-2xl font-bold'>
+															{item.heading}
+														</h2>
+														<p className='text-xl lg:text-2xl font-bold leading-8 lg:leading-10 p-3'>
+															{item.paragraph}
+														</p>
+													</div>
+												</div>
+											))}
+										</div>
 
 										<button
 											onClick={() => setIsOpen(false)}
