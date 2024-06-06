62059ca6352d4914d1c7962ad97c059f478a039d
latz09 <85132328+latz09@users.noreply.github.com>
2024-06-05 21:49:47 -0500

    thank-you page

--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -1,23 +1,44 @@
 import DynamicHello from '@/components/thank-you-page/DynamicHello';
 import { MainHeading } from '@/components/utils/Typography';
 import { Suspense } from 'react';
+
+
 export const metadata = {
 	title: 'Thank You!',
 };
 
 const ThankYouPage = () => {
-	return (
-		<div className='max-w-5xl mx-auto h-[70vh] lg:h-[75vh] '>
-			<div className='grid place-items-center h-full'>
-				<div className='grid place-items-center gap-6'>
-					<MainHeading title='Thank You!' type='dark' />
-					<Suspense fallback={<div className="hidden">Loading...</div>}>
-						<DynamicHello />
-					</Suspense>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-export default ThankYouPage;
+    return (
+      <div className='max-w-5xl mx-auto h-[70vh] lg:h-[75vh]'>
+        <div className='grid place-items-center h-full'>
+          <div className='flex flex-col gap-6'>
+            <div className='h-1/3'>
+            
+                
+              <MainHeading title='Thank You!' type='dark' />
+            </div>
+            <div className='h-2/3'>
+              <Suspense fallback={<div className='h-full'><Placeholder /></div>}>
+                <DynamicHello />
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  const Placeholder = () => (
+    <div className='grid place-items-center h-full'>
+      <div className='text-center grid gap-3'>
+        <div className='h-8 w-40 bg-gray-200 rounded'></div>
+        <div className='h-4 w-60 bg-gray-200 rounded mt-2'></div>
+        <div className='h-4 w-40 bg-gray-200 rounded mt-2'></div>
+        <div className='mt-8'>
+          <div className='h-10 w-40 bg-gray-200 rounded'></div>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default ThankYouPage;
--- components/thank-you-page/DynamicHello.js
+++ components/thank-you-page/DynamicHello.js
@@ -1,29 +1,46 @@
 'use client';
 
-
 import { useSearchParams } from 'next/navigation';
 import { SubHeading } from '../utils/Typography';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const DynamicHello = () => {
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name') || 'there';
+	const searchParams = useSearchParams();
+	const name = searchParams.get('name') || 'there';
 
-  return (
-    <div className='grid place-items-center '>
-      <div className='text-center grid gap-3'>
-        <SubHeading title={`Thank you, ${name}, for your request`} />
-        <p className='text-xl lg:text-2xl'>
-          We will get back to you shortly
-        </p>
-        <div className='mt-8'>
-          <Link href='/'>
-            <span className='text-xl font-semibold px-8 py-4 border border-primary/40 sm:hover:bg-dark/10 sm:hover:border-none sm:hover:scale-95 transition duration-700'>Back to Home</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<motion.div
+			className='grid place-items-center h-full'
+			initial={{ opacity: 0, scale: 0.8 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{ duration: 1.1 }}
+		>
+			<div className='text-center grid gap-3'>
+				<SubHeading title={`Thank you, ${name}, for your request`} />
+				<p className='text-xl lg:text-2xl'>We will get back to you shortly</p>
+				<motion.div className='font-bold opacity-80 tracking-wide lg:text-lg '
+                initial={{ opacity: 0, x: 50}}
+                animate={{ opacity: 1, x: 0}}
+                transition={{ duration: 1.2, delay: 1.8 }}
+                >
+					-Katie Jo
+				</motion.div>
+				<motion.div
+					className='mt-8'
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 0.75, delay: 0.55 }}
+				>
+					<Link href='/'>
+						<span className='text-xl font-semibold px-8 py-4 border border-primary/40 sm:hover:bg-dark/10 sm:hover:border-none sm:hover:scale-95 transition duration-700'>
+							Back to Home
+						</span>
+					</Link>
+				</motion.div>
+			</div>
+		</motion.div>
+	);
 };
 
 export default DynamicHello;
