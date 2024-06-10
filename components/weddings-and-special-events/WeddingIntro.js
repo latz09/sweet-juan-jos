f82db0bf39437a6a9c51ee282dcf048bc42d94bf
latz09 <85132328+latz09@users.noreply.github.com>
2024-06-10 13:50:46 -0500

    d

--- components/weddings-and-special-events/WeddingIntro.js
+++ components/weddings-and-special-events/WeddingIntro.js
@@ -5,7 +5,7 @@ const WeddingIntro = ({ data }) => {
 	return (
 		<div className='grid gap-6 lg:gap-8 max-w-5xl mx-auto'>
 			
-				<MainHeading title={data.heading} type='dark' />
+				
 			
 
 			<div className='lg:w-2/3 mx-auto grid place-items-center gap-6 text-xl lg:text-2xl font-bold text-center leading-9 lg:leading-10 px-2 '>
