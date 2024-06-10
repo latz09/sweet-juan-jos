1b9491205408c7b724e4a2f810ccfb226d720812
latz09 <85132328+latz09@users.noreply.github.com>
2024-06-10 17:19:54 -0500

    finalized itemsdisplay

--- app/available-display-items/page.js
+++ app/available-display-items/page.js
@@ -1,6 +1,6 @@
 import { sanityClient } from '@/lib/sanityConnection';
 import { FETCH_AVAILABLE_DISPLAY_ITEMS_QUERY } from '@/data/queries/FETCH_AVAILABLE_DISPLAY_ITEMS_QUERY';
-import { SubHeading } from '@/components/utils/Typography';
+import { MainHeading } from '@/components/utils/Typography';
 import AvailableDisplayItems from '@/components/DisplayItems/AvailableDisplayItems';
 import AnimateUp from '@/components/utils/animations/AnimateUp';
 
@@ -14,7 +14,7 @@ const AvailableDisplayItemsPage = async () => {
 
 	return (
 		<div className='pt-4 lg:pt-16 grid gap-12 lg:gap-16 bg-gradient-to-b from-light via-primary/20 to-light'>
-			<SubHeading title='Available Display Items' />
+			<MainHeading title='Available Display Items' />
 			<AnimateUp>
 				<AvailableDisplayItems data={data} />
 			</AnimateUp>
