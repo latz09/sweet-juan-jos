9340eb5df0faaba56958234cee5604d2243e384b
latz09 <85132328+latz09@users.noreply.github.com>
2024-06-11 12:37:58 -0500

    fix link

--- components/special-events/MenuItem.js
+++ components/special-events/MenuItem.js
@@ -7,7 +7,7 @@ const MenuItem = ({ title, image, link }) => {
 	return (
 		<li className=' grid place-items-center  '>
 			<div className='relative lg:hover:scale-95 shadow-lg lg:rounded-lg shadow-primary/30 lg:hover:shadow-primary/80 transition duration-700  ' key={title}>
-				<Link href={`/services/${link}`} className="group">
+				<Link href={`${link}`} className="group">
 					<Image
 						src={image}
 						alt={title}
--- components/utils/StartPlanningLink.js
+++ components/utils/StartPlanningLink.js
@@ -3,9 +3,9 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 const StartPlanningLink = () => {
 	return (
 		<Link href='/contact-katie-jo'>
-			<div className='text-primary px-4 py-2 rounded-sm group flex items-center cursor-pointer transition duration-300 text-xl lg:text-2xl font-bold text-center '>
-				<span className='group-hover:text-dark '>Start Planning</span>
-				<FaLongArrowAltRight className='pl-2 transform transition-transform duration-300 text-dark group-hover:translate-x-4 group-hover:text-primary' />
+			<div className='text-primary px-4 py-2 rounded-sm group flex items-center cursor-pointer  text-xl lg:text-2xl font-bold text-center '>
+				<span className='lg:group-hover:text-dark '>Start Planning</span>
+				<FaLongArrowAltRight className='pl-2 transform transition-transform duration-300 text-dark lg:group-hover:translate-x-4 lg:group-hover:text-primary' />
 			</div>
 		</Link>
 	);
