71b79d6f8866ef25d0fe2cdf6f272f5a79cef98d
latz09 <85132328+latz09@users.noreply.github.com>
2024-06-11 11:51:04 -0500

    d

--- components/special-events/AvailableDisplayItemsBlurb.js
+++ components/special-events/AvailableDisplayItemsBlurb.js
@@ -4,28 +4,13 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 import Link from 'next/link';
 
 const AvailableDisplayItemsBlurb = ({ data }) => {
-	
 	return (
 		<Link href={'/available-display-items'}>
-			<div className='py-16 grid lg:grid-cols-4 gap-12 place-items-center  max-w-5xl mx-auto '>
-				<div className='lg:col-span-3 grid gap-2 place-items-center '>
+			<div className='py-16 grid  gap-12 place-items-center  max-w-5xl mx-auto '>
+				<div className='grid gap-2 place-items-center '>
 					<MainHeading title={data.heading} />
 					<SubHeading title={data.subHeading} />
-
-					<div className='hidden lg:block'>
-						<ViewOurInventory />
-					</div>
-				</div>
-				<div className='grid place-items-center'>
-					<Image
-						src={data.availableDisplayItemsImage}
-						alt='Available Display Items'
-						width={200}
-						height={200}
-					/>
-					<div className='block lg:hidden'>
-						<ViewOurInventory />
-					</div>
+					<ViewOurInventory />
 				</div>
 			</div>
 		</Link>
@@ -37,10 +22,10 @@ export default AvailableDisplayItemsBlurb;
 const ViewOurInventory = () => {
 	return (
 		<div className=' flex items-center gap-4 group p-4'>
-			<span className='text-xl lg:text-2xl font-bold transform transition-transform duration-300 group-hover:text-primary'>
+			<span className='text-xl lg:text-2xl font-bold text-primary transform transition-transform duration-300 group-hover:text-dark'>
 				View Our Inventory
 			</span>
-			<span className='pl-2 transform transition-transform duration-300 group-hover:translate-x-4'>
+			<span className='pl-2 transform transition-transform duration-300 group-hover:translate-x-4 group-hover:text-primary'>
 				<FaLongArrowAltRight />
 			</span>
 		</div>
--- components/utils/StartPlanningLink.js
+++ components/utils/StartPlanningLink.js
@@ -5,7 +5,7 @@ const StartPlanningLink = () => {
 		<Link href='/contact-katie-jo'>
 			<div className='text-primary px-4 py-2 rounded-sm group flex items-center cursor-pointer transition duration-300 text-xl lg:text-2xl font-bold text-center '>
 				<span className='group-hover:text-dark '>Start Planning</span>
-				<FaLongArrowAltRight className='pl-2 transform transition-transform duration-300 group-hover:translate-x-4' />
+				<FaLongArrowAltRight className='pl-2 transform transition-transform duration-300 text-dark group-hover:translate-x-4 group-hover:text-primary' />
 			</div>
 		</Link>
 	);
