b1db03eb8f4abcf760531a6ff2ea5005f6c1f10d
latz09 <85132328+latz09@users.noreply.github.com>
2024-07-24 14:54:50 -0500

    add back in page animate

--- components/utils/animations/PageEntry.js
+++ components/utils/animations/PageEntry.js
@@ -1,18 +1,18 @@
 'use client';
 
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 const PageEntry = ({ children, className }) => {
 	return (
 		<div>
-			<div
-				// initial={{ opacity: 0 }}
-				// animate={{ opacity: 1 }}
-				// exit={{ opacity: 0 }}
-				// transition={{ duration: 1.1 }}
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				transition={{ duration: 1.1 }}
 				className={className}
 			>
 				{children}
-			</div>
+			</motion.div>
 		</div>
 	);
 };
