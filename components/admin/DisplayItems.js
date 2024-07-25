d22d5b2c4f570c5fe7eeae99ff8aa6737448a87d
latz09 <85132328+latz09@users.noreply.github.com>
2024-07-25 14:31:39 -0500

    notes and display items on admin contact form

--- components/admin/DisplayItems.js
+++ components/admin/DisplayItems.js
@@ -1,29 +1,29 @@
 import Image from 'next/image';
 
 const DisplayItems = ({ data }) => {
-  return (
-    <div>
-      <h2 className='text-2xl font-bold mb-8 '>Selected Display Items</h2>
-      <div className="grid lg:grid-cols-2 place-items-center">
-      {data.map(item => (
-        <div key={item._id} className='flex items-center mb-4'>
-          <div className='relative w-32 h-32 mr-4'>
-            <Image
-              src={item.imageUrl}
-              alt={item.title}
-              layout='fill'
-              objectFit='cover'
-              className='rounded'
-            />
-          </div>
-          <div>
-            <h3 className='text-xl font-bold'>{item.inventoryID}</h3>
-          </div>
-        </div>
-      ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className='text-2xl font-bold mb-8 '>Selected Display Items</h2>
+			<div className='grid grid-cols-2 lg:grid-cols-3 place-items-center'>
+				{data.map((item) => (
+					<div key={item._id} className='grid place-items-center gap-3'>
+					
+							<Image
+								src={item.imageUrl}
+								alt={item.title}
+								width={150}
+                                height={150}
+								className='rounded-sm shadow-lg'
+							/>
+						
+						<div>
+							<h3 className='text-xl font-bold'>{item.inventoryID}</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default DisplayItems;
