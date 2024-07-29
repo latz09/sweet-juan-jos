c2c3fb68817c3644b8b118a0a8c148e62ba0b598
latz09 <85132328+latz09@users.noreply.github.com>
2024-07-29 13:50:32 -0500

    wedding intro

--- components/weddings-and-special-events/WeddingIntro.js
+++ components/weddings-and-special-events/WeddingIntro.js
@@ -3,7 +3,7 @@ import StartPlanningLink from '../utils/StartPlanningLink';
 const WeddingIntro = ({ data }) => {
 	return (
 		<div className='grid  max-w-5xl mx-auto'>
-			<div className='w-4/5 lg:w-2/3 mx-auto grid place-items-center gap-6 text-xl lg:text-2xl font-bold text-center leading-9 lg:leading-10 px-2 '>
+			<div className='w-5/6 lg:w-2/3 mx-auto grid place-items-center gap-6 text-xl lg:text-2xl font-bold text-center leading-9 lg:leading-10 px-2 '>
 				<p>{data.openingParagraph} </p>
 				<StartPlanningLink />
 			</div>
