1a297af25720f5f61343a7982302bc2198242095
latz09 <85132328+latz09@users.noreply.github.com>
2024-07-29 14:24:44 -0500

    terms and conditions modal animation

--- components/contact/ReadTermsAndConditions.js
+++ components/contact/ReadTermsAndConditions.js
@@ -1,6 +1,10 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import logo from '@/public/images/logo/transparent-juanjos.png';
+import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
+import AnimateUp from '../utils/animations/AnimateUp';
 
 const ReadTermsAndConditions = ({ data }) => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,56 +31,80 @@ const ReadTermsAndConditions = ({ data }) => {
 	return (
 		<>
 			<button
-            type = 'button'
+				type='button'
 				className='font-black text-xl text-primary px-4 py-2 rounded'
 				onClick={openModal}
-
 			>
-				 Terms and Conditions
+				Terms and Conditions
 			</button>
 
-			{isModalOpen && (
-				<div className='fixed inset-0 bg-dark/80 flex justify-center items-center z-50'>
-					<div className='bg-light rounded-lg shadow-lg max-w-2xl mx-auto p-6 overflow-y-auto max-h-full'>
-						<div className='flex justify-between items-center border-b pb-3'>
-							<h2 className='text-2xl font-semibold'>Terms and Conditions</h2>
-							<button
-								className='text-gray-500 hover:text-gray-700'
-								onClick={closeModal}
-							>
-								✖
-							</button>
-						</div>
-						<div className='mt-4 grid'>
-							<div className='py-7'>
-								{data.intro.map((paragraph, index) => (
-									<p key={index} className='mb-2 font-bold text-center text-lg'>
-										{paragraph}
-									</p>
-								))}
+			<AnimatePresence>
+				{isModalOpen && (
+					<motion.div
+						className='fixed inset-0 bg-primary sm:bg-primary/70 backdrop-blur-sm flex justify-center items-center z-50 p-2'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition = {{ duration: .5 }}
+					>
+						<motion.div
+							className='bg-light rounded-lg shadow-lg max-w-2xl mx-auto p-6 overflow-y-auto max-h-full '
+							initial={{ scale: 0.8 }}
+							animate={{ scale: 1 }}
+							exit={{ scale: 0.8 }}
+							transition = {{ type: 'spring', stiffness: 260, damping: 20, duration: 1.1 }}
+						>
+							<div className='flex justify-between items-center border-b border-primary pb-3'>
+								<h2 className='text-2xl font-semibold'>Terms and Conditions</h2>
+								<button
+									
+									onClick={closeModal}
+								>
+									✖
+								</button>
 							</div>
-							{data.terms.map((term, index) => (
-								<div key={index} className='my-4'>
-									<h3 className='text-xl font-semibold'>{term.title}</h3>
-									{term.description.map((desc, i) => (
-										<p key={i} className='ml-4'>
-											{desc}
+							<div className='pt-6  opacity-70'>
+								<Image
+									src={logo}
+									alt='Juanjos Logo'
+									width={100}
+									height={100}
+									className='mx-auto'
+								/>
+							</div>
+							<div className='grid'>
+								<div className='py-4'>
+									{data.intro.map((paragraph, index) => (
+										<p key={index} className='mb-2 font-bold text-center text-lg'>
+											{paragraph}
 										</p>
 									))}
 								</div>
-							))}
-						</div>
-						<div className='flex justify-between items-center border-b py-6'>
-							<button
-								className='px-4 py-2 bg-primary text-light font-bold w-full text-2xl rounded'
-								onClick={closeModal}
-							>
-								Close
-							</button>
-						</div>
-					</div>
-				</div>
-			)}
+								{data.terms.map((term, index) => (
+									<AnimateUp key={index}>
+										<div className='my-4'>
+											<h3 className='text-xl font-semibold'>{term.title}</h3>
+											{term.description.map((desc, i) => (
+												<p key={i} className='ml-4'>
+													{desc}
+												</p>
+											))}
+										</div>
+									</AnimateUp>
+								))}
+							</div>
+							<div className='flex justify-between items-center border-b border-primary py-6'>
+								<button
+									className='px-4 py-2 bg-primary text-light font-bold w-full text-2xl rounded'
+									onClick={closeModal}
+								>
+									Close
+								</button>
+							</div>
+						</motion.div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 };
--- data/services/candy-and-treats.js
+++
@@ -1,9 +0,0 @@
-import landingImage from '../../public/images/ai-generated/krispies.webp';
-
-export const candyAndTreatsData = {
-	heading: 'Candy & Treats',
-	subheading: 'Crafted Confections.  Unique Twists on Beloved Classics',
-	img: landingImage,
-	CTA: '/sweet-juanjos/contact-us',
-	CTAText: 'Request Order Now',
-};
