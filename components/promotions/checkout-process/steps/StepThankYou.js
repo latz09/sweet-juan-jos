5d99e2338e4e49729b2a414a2091d85511e8fcdc
latz09 <85132328+latz09@users.noreply.github.com>
2025-01-26 16:25:43 -0600

    auto response email

--- components/promotions/checkout-process/steps/StepThankYou.js
+++ components/promotions/checkout-process/steps/StepThankYou.js
@@ -9,12 +9,18 @@ const StepThankYou = ({ onClose }) => {
 				<h3 className='text-2xl font-semibold text-green-600 mb-4'>
 					Thank You for Your Order!
 				</h3>
-				<p className='text-dark/80  max-w-3xl mx-auto font-bold tracking-wider'>
+				<p className='text-dark/80 max-w-3xl mx-auto font-bold tracking-wider'>
 					Thank you for your order! Payment will be collected at the time of
-					delivery or pickup, depending on the option you selected. We
-					appreciate your business and look forward to serving you!
+					{` `}
+					<span className='text-primary font-semibold'>delivery or pickup</span>
+					, depending on the option you selected. We appreciate your business
+					and look forward to serving you!
 				</p>
-                <p className="mb-12 mt-4 text-xl font-bold text-primary">-Katie Jo</p>
+				<p className='mt-4 text-dark/60 max-w-3xl mx-auto tracking-wider'>
+					{`An email confirmation has been sent to your inbox. Please check your
+					spam or junk folder if you don’t see it in your inbox.`}
+				</p>
+				<p className='mb-12 mt-4 text-xl font-bold text-primary'>-Katie Jo</p>
 				<button
 					className='px-4 py-2 bg-primary text-light font-bold rounded-sm shadow-lg hover:bg-primary-dark transition'
 					onClick={onClose} // Close the modal
