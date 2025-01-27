3a936cc2ce3a875066c9d057a0ab74fbff690a79
latz09 <85132328+latz09@users.noreply.github.com>
2025-01-27 14:29:37 -0600

    f

--- components/promotions/checkout-process/steps/StepThankYou.js
+++ components/promotions/checkout-process/steps/StepThankYou.js
@@ -9,14 +9,14 @@ const StepThankYou = ({ onClose }) => {
 				<h3 className='text-2xl font-semibold text-green-600 mb-4'>
 					Thank You for Your Order!
 				</h3>
-				<p className='text-dark/80 max-w-3xl mx-auto font-bold tracking-wider text-lg'>
+				<p className='text-dark/80 max-w-3xl mx-auto font-bold tracking-wider text-xl lg:text-2xl'>
 					Payment will be collected at the time of
 					{` `}
 					<span className='text-primary font-semibold'>delivery or pickup</span>
 					, depending on the option you selected. We appreciate your business
 					and look forward to baking for you!
 				</p>
-				<p className='mt-4 text-dark/90 max-w-3xl mx-auto tracking-wider'>
+				<p className='mt-4 text-xl  text-dark/90 max-w-3xl mx-auto tracking-wider'>
 					{`An email confirmation has been sent to your inbox. Please check your
 					spam or junk folder if you don't see it in your inbox.`}
 				</p>
