8d2fa680f498a14410fc7991c79789333cb2191c
latz09 <85132328+latz09@users.noreply.github.com>
2025-11-06 20:48:08 -0600

    popupmanager off

--- app/layout.js
+++ app/layout.js
@@ -48,7 +48,7 @@ const RootLayout = async ({ children }) => {
 
 
 				
-				<PopUpManager promotion={activePromotion} acceptingOrders={acceptingOrders} />
+				{/* <PopUpManager promotion={!activePromotion} acceptingOrders={acceptingOrders} /> */}
 				<div className='max-w-7l mx-auto '>
 					
 					<div>{children}</div>
